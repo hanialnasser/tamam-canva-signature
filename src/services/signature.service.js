ebe416e6886991435cf25d6d4ef0718b5f198d22
Haris arain <harisarain704@gmail.com>
2023-07-25 12:26:27 +0500

    increase the height of canvas

--- src/services/signature.service.js
+++ src/services/signature.service.js
@@ -1,27 +1,22 @@
-
-const {createCanvas} = require('canvas')
-
+const { createCanvas } = require("canvas");
 
 // drawing canvas signature by giving x, y coordinates  of array of objects
 
 const drawCanvasSignature = async (signatures) => {
-
-  const canvas = createCanvas(400, 250)
-  const context = canvas.getContext('2d')
-  context.lineWidth = 1.5
-  context.strokeStyle = 'black'
+  const canvas = createCanvas(400, 350);
+  const context = canvas.getContext("2d");
+  context.lineWidth = 1.5;
+  context.strokeStyle = "black";
   for (const signature of signatures) {
     context.beginPath();
     context.moveTo(signature[0].x, signature[0].y);
     for (let i = 1; i < signature.length; i++) {
       context.lineTo(signature[i].x, signature[i].y);
     }
-     context.stroke()
+    context.stroke();
   }
-  return canvas.toDataURL()
-}
-
-
+  return canvas.toDataURL();
+};
 
 module.exports = {
   drawCanvasSignature,
